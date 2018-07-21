df6a2b91da0ac76269db1305caeca701f303e507
James Shin <jhscode@gmail.com>
2018-07-21 19:42:22 -0400

    removed listjob

--- src/Components/ListJob.js
+++ src/Components/ListJob.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Job from './Job';
 
 class ListJob extends React.Component {
   render() {
     return (
       <div>
         <div>List of all Jobs</div>
-        <Job />
       </div>
     );
   }
